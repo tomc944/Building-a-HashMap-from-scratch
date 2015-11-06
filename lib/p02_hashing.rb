cae63e69045d1242cb55ebb9df8e8f4f7c554af5
App Academy Student <thomas.claiborne3@gmail.com>
2015-11-06 14:09:50 -0800

    Hashing

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,15 +4,38 @@ end
 
 class Array
   def hash
+    if empty?
+      return 685041758907420958468234058492758902375.hash
+    end
+    sum = 0
+    each_with_index do |value, index|
+      current = value % (index + 1)
+      sum += current.hash
+    end
+    sum
   end
 end
 
 class String
   def hash
+    ascii_nums = self.split("").map do |char|
+      char.ord
+    end
+    ascii_nums.hash
+
   end
 end
 
 class Hash
   def hash
+    if empty?
+      return 7890371589037590137571309857358490277.hash
+    end
+    sum = 0
+    self.each do |key, value|
+      current = key.hash + value.hash
+      sum += current
+    end
+    sum
   end
 end
